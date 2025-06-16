9b2eb66dfb7da7cf11815d8856222e87c711f58a
sintharic <chris297@gmx.de>
2025-06-16 21:55:29 +0200

    added option to only include some of the information in the Graph

--- make_graph.py
+++ make_graph.py
@@ -2,6 +2,8 @@ ontofile = 'ontology.json'
 kwfile = 'keywords.json'
 jsonfile = 'SPP.json'
 vault = 'SPP-KG'
+add_info = ['people', 'methods', 'documentation']
+# add_info = ['people', 'documentation']
 
 
 
@@ -55,30 +57,31 @@ for project in projects:
 os.makedirs(vault, exist_ok=True)
 os.makedirs(f'{vault}{os.sep}projects', exist_ok=True)
 os.makedirs(f'{vault}{os.sep}topics', exist_ok=True)
-os.makedirs(f'{vault}{os.sep}people', exist_ok=True)
-os.makedirs(f'{vault}{os.sep}methods', exist_ok=True)
-os.makedirs(f'{vault}{os.sep}documentation', exist_ok=True)
+if 'people' in add_info: os.makedirs(f'{vault}{os.sep}people', exist_ok=True)
+if 'methods' in add_info: os.makedirs(f'{vault}{os.sep}methods', exist_ok=True)
+if 'documentation' in add_info: os.makedirs(f'{vault}{os.sep}documentation', exist_ok=True)
 
 
 # add people to vault
-for person in people:
-  filename = f"{vault}{os.sep}people{os.sep}{person['name']}.md"
-  with open(filename, 'w') as file:
-    file.write('## Full Name\n')
-    file.write(f"{person['title']} {person['name']}\n")
-    file.write('\n## Affiliations\n')
-    for affil in person['affiliations']:
-      file.write(f'- {affil}\n')
-    num = int(person['project'][-2:])
-    # print(num)#DEBUG
-    file.write(f'- [[{project_string}{str(num).zfill(2)}]]')
-    for project in projects:
-      if project['alphabetical_number'] == num:
-        project['researchers'].append(f"{person['name']}")
-    file.write('\n## Contact\n')
-    if person['phone']: file.write(f"- {person['phone']}\n")
-    if person['email']: file.write(f"- {person['email']}\n")
-    if person['website']: file.write(f"- {person['website']}\n")
+if 'people' in add_info:
+  for person in people:
+    filename = f"{vault}{os.sep}people{os.sep}{person['name']}.md"
+    with open(filename, 'w') as file:
+      file.write('## Full Name\n')
+      file.write(f"{person['title']} {person['name']}\n")
+      file.write('\n## Affiliations\n')
+      for affil in person['affiliations']:
+        file.write(f'- {affil}\n')
+      num = int(person['project'][-2:])
+      # print(num)#DEBUG
+      file.write(f'- [[{project_string}{str(num).zfill(2)}]]')
+      for project in projects:
+        if project['alphabetical_number'] == num:
+          project['researchers'].append(f"{person['name']}")
+      file.write('\n## Contact\n')
+      if person['phone']: file.write(f"- {person['phone']}\n")
+      if person['email']: file.write(f"- {person['email']}\n")
+      if person['website']: file.write(f"- {person['website']}\n")
 
 
 # add projects to vault
@@ -97,14 +100,14 @@ for project in projects:
       loc = project['locations'][idx]
       file.write(f'{PI} ({loc})\n')
     
-    if 'methods' in project.keys():
+    if ('methods' in project.keys()) and 'methods' in add_info:
       file.write('\n## Methods\n')
       for item in project['methods']:
         file.write(f'- [[{item}]]\n')
     else:
       project['methods'] = []
     
-    if 'documentation_media' in project.keys():
+    if ('documentation_media' in project.keys()) and ('documentation' in add_info):
       file.write('\n## Documentation\n')
       for item in project['documentation_media']:
         file.write(f'- [[{item}]]\n')
@@ -147,15 +150,19 @@ for project in projects:
     doc_used_by[doc].append(str(project['alphabetical_number']).zfill(2))
 
 methods = set(methods)
-for method in methods:
-  filename = f'{vault}{os.sep}methods{os.sep}{method}.md'
-  with open(filename,'w') as file:
-    for pid in method_used_by[method]:
-      file.write(f'- [[{project_string}{pid}]]\n')
-
 docs = set(docs)
-for doc in docs:
-  filename = f'{vault}{os.sep}documentation{os.sep}{doc}.md'
-  with open(filename, 'w') as file:
-    for pid in doc_used_by[doc]:
-      file.write(f'- [[{project_string}{pid}]]\n')
+
+
+if 'methods' in add_info:
+  for method in methods:
+    filename = f'{vault}{os.sep}methods{os.sep}{method}.md'
+    with open(filename,'w') as file:
+      for pid in method_used_by[method]:
+        file.write(f'- [[{project_string}{pid}]]\n')
+
+if 'documentation' in add_info:
+  for doc in docs:
+    filename = f'{vault}{os.sep}documentation{os.sep}{doc}.md'
+    with open(filename, 'w') as file:
+      for pid in doc_used_by[doc]:
+        file.write(f'- [[{project_string}{pid}]]\n')
